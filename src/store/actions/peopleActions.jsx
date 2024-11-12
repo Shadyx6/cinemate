fac9ff64e8ded8265024f4e74c417462d4a6813f
Shadyx6 <shadkhanxox@gmail.com>
2024-11-12 16:44:50 +0530

    People individual page Ui done minor changes left

--- src/store/actions/peopleActions.jsx
+++ src/store/actions/peopleActions.jsx
@@ -6,15 +6,16 @@ export const asyncGetPeople = (id) => async (dispatch, getState) => {
         const details = await axios.get(`/person/${id}`);
         // const recommendations = await axios.get(`/person/${id}/recommendations`);
         // const latest = await axios.get(`/person/${id}/latest`);
-        const combinedCredits = await axios.get(`/person/${id}/credits`);
+        const movies = await axios.get(`/person/${id}/credits`);
         const images = await axios.get(`/person/${id}/images`);
-        // const watchProviders = await axios.get(`/person/${id}/watch/providers`);
+        const taggedImages = await axios.get(`/person/${id}/tagged_images`);
         const externalIds = await axios.get(`/person/${id}/external_ids`);
         const peopleData = {
             details: details.data,
             images: images.data,
-            combinedCredits: combinedCredits.data,
-            externalIds: externalIds.data
+            movies: movies.data,
+            externalIds: externalIds.data,
+            taggedImages: taggedImages.data.results,
         }
         console.log(peopleData)
         dispatch(getPeople(peopleData));
