e6b656a2089e86a09e3ee5bf0035c7f546a662bf
Shadyx6 <shadkhanxox@gmail.com>
2024-12-09 10:04:41 +0530

    Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Routing from './utils/Routing'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
   <>
   <div className="h-full w-full bg-black text-[#D1D1D1] ">
         <Routing />
+        <Analytics />
     </div>
   </>
   )
