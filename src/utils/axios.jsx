751e3aaed6d1eb48e6ec281737380585616a1abf
Shadyx6 <shadkhanxox@gmail.com>
2024-12-11 15:01:53 +0530

    API issues may be resolved now

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const apiKey = import.meta.env.VITE_TMDB_API_KEY;
-
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${apiKey}`
+        Authorization: import.meta.env.VITE_TMDB_API_KEY
       }
 })
 
