29b34e0bd19c4f752e28a2cd8791cc3b97f96aad
Shadyx6 <shadkhanxox@gmail.com>
2024-12-11 14:32:16 +0530

    Deployment bug fixed

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
         accept: 'application/json',
-        Authorization: import.meta.env.VITE_TMDB_API_KEY
+        Authorization: `Bearer ${apiKey}`
       }
 })
 
